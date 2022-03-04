3ec9f3e3896aadb7cf0763c65db8efa83008b2c1
Indiance <tharun.tvishy@gmail.com>
2022-03-04 19:40:17 +0530

    added some more profiles for the display

--- src/components/Profilefeed.jsx
+++ src/components/Profilefeed.jsx
@@ -1,12 +1,30 @@
+import React from "react";
+
 const ProfileFeed = () => {
 	return (
-	<div class="noteCard my-2 mx-2 card">
-		<div class="card-body">
-			<h5 class="card-title">Username</h5>
-			<p class="card-text">Degree</p>
-			<button onclick="deleteNote(this.id)" class="btn btn-primary">Create DM</button>
+		<React.Fragment>
+			<div class="noteCard my-2 mx-2 card">
+				<div class="card-body">
+					<h5 class="card-title">Indiance</h5>
+					<p class="card-text">Computer Engineering</p>
+					<button onclick="deleteNote(this.id)" class="btn btn-primary">Create DM</button>
+				</div>
 			</div>
-		</div>
+			<div class="noteCard my-2 mx-2 card">
+				<div class="card-body">
+					<h5 class="card-title">Realtime</h5>
+					<p class="card-text">Data Science</p>
+					<button onclick="deleteNote(this.id)" class="btn btn-primary">Create DM</button>
+				</div>
+			</div>
+			<div class="noteCard my-2 mx-2 card">
+				<div class="card-body">
+					<h5 class="card-title">AR</h5>
+					<p class="card-text">Computer Science</p>
+					<button onclick="deleteNote(this.id)" class="btn btn-primary">Create DM</button>
+				</div>
+			</div>
+		</React.Fragment>
 		)
 }
 export default ProfileFeed;
