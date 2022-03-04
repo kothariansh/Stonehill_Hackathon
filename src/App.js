a15b6d9c38c1da50f54b9827ac3a2ac4766526d1
Indiance <tharun.tvishy@gmail.com>
2022-03-04 14:59:22 +0530

    amending login form to work. todo: hide the api keys

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
-import ChatFeed from './components/ChatFeed'
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 const App = () => {
+	if (!localStorage.getItem('username')) return <LoginForm />;
 	return (
 		<ChatEngine
 			height="100vh"
